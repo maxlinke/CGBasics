fd4517e1f76196fdaf88950fb875b068cd583e5a
Max Linke <maxlinke@users.noreply.github.com>
2019-11-25 20:28:23 +0100

    working on automated tests

--- Assets/Scripts/temp or test/ExpressionTester.cs
+++ Assets/Scripts/temp or test/ExpressionTester.cs
@@ -9,24 +9,35 @@ public class ExpressionTester : MonoBehaviour {
 
     void Start () {
         outputField.text = GetFunctionHelper();
+        
 
         inputField.onEndEdit.AddListener((input) => {Dictionary<string, float> vars = new Dictionary<string, float>();
-            vars.Add("x", 1);
-            vars.Add("y", 2);
-            vars.Add("z", 3);
+            // vars.Add("x", 1);
+            // vars.Add("y", 2);
+            // vars.Add("z", 3);
 
-            var outputString = string.Empty;
-            int testCount = 16 * 6;
-            for(int i=0; i<testCount; i++){        //simulating all the matrices
-                try{
-                    string numberToParse = input + "+" + (testCount - i - 1).ToString();
-                    outputString = StringExpressions.ParseExpression(numberToParse, vars).ToString();
-                }catch(System.Exception e){
-                    outputString = $"{e.Message}\n{e.StackTrace}";
+            // var outputString = string.Empty;
+            // int testCount = 16 * 6;
+            // for(int i=0; i<testCount; i++){        //simulating all the matrices
+            //     try{
+            //         string numberToParse = input + "+" + (testCount - i - 1).ToString();
+            //         outputString = StringExpressions.ParseExpression(numberToParse, vars).ToString();
+            //     }catch(System.Exception e){
+            //         outputString = $"{e.Message}\n{e.StackTrace}";
+            //     }
+            // }
+            // outputField.text = $"{outputString}\n\n{GetFunctionHelper()}\nframe{Time.frameCount}";
+
+            outputField.text = "";
+            for(int i=0; i<35; i++){
+                string generated = GenerateFloatingPointNumberString();
+                if(StringExpressions.TryParseExpression(generated, out var parsed)){
+                    outputField.text += $"{generated} -> {parsed.ToString()}";
+                }else{
+                    outputField.text += $" >>> Error <<< {generated}";
                 }
+                outputField.text += "\n";
             }
-            outputField.text = $"{outputString}\n\n{GetFunctionHelper()}\nframe{Time.frameCount}";
-            // outputString = ExpressionHandler.ParseExpression(input, vars).ToString();
         });
     }
 
@@ -50,6 +61,48 @@ public class ExpressionTester : MonoBehaviour {
             sb.Append($" \t{descs[i]}\n");
         }
         return sb.ToString();
+    }
+
+    string VarsToNiceString (Dictionary<string, float> variables) {
+        int maxVarNameLength = 0;
+        string[] varNames = new string[variables.Keys.Count];
+        float[] varValues = new float[variables.Values.Count];
+        int i = 0;
+        foreach(var varName in variables.Keys){
+            varNames[i] = varName;
+            varValues[i] = variables[varName];
+            maxVarNameLength = Mathf.Max(maxVarNameLength, varName.Length);
+            i++;
+        }
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        for(i=0; i<varNames.Length; i++){
+            int varNameLength = varNames[i].Length;
+            sb.Append(varNames[i]);
+            for(int j=0; j<maxVarNameLength-varNameLength; j++){
+                sb.Append(" ");
+            }
+            sb.Append($" \t{varValues[i]}\n");
+        }
+        return sb.ToString();
+    }
+
+    string GenerateTestString (int numberOfOperands, Dictionary<string, float> variables = null, bool whiteSpaceEverywhere = true, int maxFunctionDepth = 3) {
+        var output = string.Empty;
+        int parenthesisLevel = 0;
+        int functionParamsToAdd = 0;
+        for(int i=0; i<numberOfOperands; i++){
+
+        }
+        return output;
+    }
+
+    string GenerateFloatingPointNumberString () {
+        var output = Random.Range(-256, +256).ToString();
+        if(Random.value > 0.5f) return output;
+        output += $".{Random.Range(0, +256)}";
+        if(Random.value > 0.5f) return output;
+        output += $"E{Random.Range(-6, 6)}";
+        return output;
     }
 	
 }

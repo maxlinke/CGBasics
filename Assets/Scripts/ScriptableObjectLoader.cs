14c58d2578f6ae3a6e8be8e8003a1b78358ead1b
Max Linke <maxlinke@users.noreply.github.com>
2019-11-27 17:22:03 +0100

    fixed the unloaded colorscheme issue
    > added a monobehaviour that simply has a serialized array of scriptable objects
    > when it is placed in a scene, those scriptableobjects get loaded into ram and are therefore findable by resources.findobjectsoftype

---
+++ Assets/Scripts/ScriptableObjectLoader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObjectLoader : MonoBehaviour {
+
+    [SerializeField] ScriptableObject[] scriptableObjectsToLoad;     // that's it... just reference them and they'll be in RAM so Resources.FindObjectsOfTypeAll can find them
+	
+}

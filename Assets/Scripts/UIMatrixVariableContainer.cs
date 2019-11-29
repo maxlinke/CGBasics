1e43558d73c882a0c6d2035c51a70d354129482a
Max Linke <maxlinke@users.noreply.github.com>
2019-11-29 01:00:28 +0100

    restructured the uimatrix prefab
    > no more extra background mask and stuff. the mask is now the object and the backgrounds are the parents. easy as that.
    > added uimatrixvariablecontainer, which will declutter everything a bit (hopefully)

---
+++ Assets/Scripts/UIMatrixVariableContainer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMatrixVariableContainer : MonoBehaviour {
+
+    [SerializeField] UIMatrix parentMatrix;
+
+    // [Header("Components")]
+
+    void Start () {
+        
+    }
+
+    void Update () {
+        
+    }
+
+    public void LoadColors (ColorScheme cs) {
+
+    }
+
+    public Dictionary<string, float> GetVariableMap () {
+        return null;
+    }
+	
+}

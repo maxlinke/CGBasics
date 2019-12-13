79d42c43fa90f02604e97f1638fba9eb4ca058d8
Max Linke <maxlinke@users.noreply.github.com>
2019-12-13 04:10:36 +0100

    made foldout tester, found bug
    > the foldout doesn't want to go up...

---
+++ Assets/Scripts/temp or test/FoldoutTester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class FoldoutTester : MonoBehaviour {
+
+    [SerializeField] Slider scaleSlider;
+    [SerializeField] Slider amountSlider;
+    [SerializeField] int minNameLength;
+    [SerializeField] int maxNameLength;
+
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.Q)){
+            var setups = new List<Foldout.ButtonSetup>();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<amountSlider.value; i++){
+                int nameLength = Random.Range(minNameLength, maxNameLength + 1);
+                sb.Append($"{i} ");
+                for(int j=0; j<nameLength; j++){
+                    sb.Append((char)(Random.Range('a', 'z' + 1)));
+                }
+                setups.Add(new Foldout.ButtonSetup(
+                    buttonName: sb.ToString(),
+                    buttonHoverMessage: string.Empty,
+                    buttonClickAction: null,
+                    buttonInteractable: Random.value < 0.5f
+                ));
+                sb.Clear();
+            }
+            Foldout.Create(setups, null, scaleSlider.value);
+        }
+    }
+	
+}
